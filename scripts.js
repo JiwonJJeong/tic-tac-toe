add6720194c231efd24e22502fae310058b88bb1
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-06-04 16:14:29 -0700

    Pseudocode general factories
    Wrap board and game manager factories in IIFE because we only want one.

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// JS script
+
+const gameboard = function board(){
+    // hold 3 private array variables for the board
+
+
+    // check win method
+        // check all 3 in a rows for X
+        // else check all 3 in a rows for O
+        // else if board is full
+            // declare tie
+
+    // add symbol (x or o) at desired location (public)
+}();
+
+const gameManager = function manager(){
+    // start game method will assign players to O or X
+
+    // run round method will prompt one player to choose a spot, then calls gameboard method to place their symbol
+    // flips players each round
+}();
+
+function player(){
+    // private variable for player name
+    // private variable for whether they are player O or X
+}
+
+// make playerO
